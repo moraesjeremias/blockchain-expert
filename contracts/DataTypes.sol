b6c7ba33c094085deb2e71dd8181f4feafb84dcc
Jeremy <jeremias@availproject.org>
2024-08-12 23:27:40 -0300

    Adds data types exercise contract

---
+++ contracts/DataTypes.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <=0.9;
+
+contract DataTypes {
+    uint8 public smallNumber = 9;
+    int8 public negativeNumber = -7;
+    address public zeroAddress = address(0);
+    bool public canEdit = true;
+}
